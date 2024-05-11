01ade8caece277e3587e19757c392f4b0021f984
AxelBrask <braskaxel@gmail.com>
2024-05-11 12:06:48 +0200

    Odom listener and wp viz

--- navigation/dead_reckoning/tf_convenience_topics/src/odom_listener.cpp
+++ navigation/dead_reckoning/tf_convenience_topics/src/odom_listener.cpp
@@ -2,78 +2,100 @@
 #include <std_msgs/msg/float64.hpp>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Transform.h>
-#include "dead_reckoning_msgs/msg/Topics.hpp"
+#include "rclcpp/executors/multi_threaded_executor.hpp"
+#include "dead_reckoning_msgs/msg/topics.hpp"
+#include "smarc_msgs/msg/dvl.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "sam_msgs/msg/topics.hpp"
 
-std_msgs::msg::Float64 current_roll, current_pitch, current_yaw, current_depth, current_x, current_y, current_u, current_v, current_w, current_p, current_q, current_r, current_alt;
-double r,p,y;
-tf2::Quaternion tfq;
+using std::placeholders::_1;
 
-void DVLCallback(const smarc_msgs::msg::DVL::SharedPtr dvl_msg)
-{
-    current_alt.data = dvl_msg->altitude;
-    // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "[ odom_listener]  Altitude from DVL: %f", dvl_msg->altitude);
-}
+class Odom_listener{
 
-void OdomCallback(const nav_msgs::msg::Odometry::SharedPtr odom_msg)
-{
-    tf2::fromMsg(odom_msg->pose.pose.orientation, tfq);
+  public:
+    rclcpp::Node *nh_;
+    rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_pitch;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_roll;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_yaw;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_depth;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_x;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_y;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_u;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_v;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_w;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_p;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_q;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feedback_r;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr feddback_alt;
 
-    tf2::Matrix3x3(tfq).getEulerYPR(y,p,r);
-    //orientation
-    current_pitch.data= p;
-    current_roll.data= r;
-    current_yaw.data= y;
-    current_depth.data= -odom_msg->pose.pose.position.z;
-    current_x.data= odom_msg->pose.pose.position.x;
-    current_y.data= odom_msg->pose.pose.position.y;
+    rclcpp::Subscription<smarc_msgs::msg::DVL>::SharedPtr dvl_sub;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
+    std_msgs::msg::Float64 current_roll, current_pitch, current_yaw, current_depth, current_x, current_y, current_u, current_v, current_w, current_p, current_q, current_r, current_alt;
+    double r,p,y;
+    tf2::Quaternion tfq;
+    Odom_listener(rclcpp::Node &nh) : nh_(&nh)
+    {
+      dvl_sub = nh_->create_subscription<smarc_msgs::msg::DVL>(
+        sam_msgs::msg::Topics::DVL_TOPIC,
+        10,
+        std::bind(&Odom_listener::DVLCallback, this, _1)
+      );
+      odom_sub = nh_->create_subscription<nav_msgs::msg::Odometry>(
+        dead_reckoning_msgs::msg::Topics::DR_ODOM_TOPIC,
+        10,
+        std::bind(&Odom_listener::OdomCallback, this, _1)
+      );
 
-    //Velocity
-    current_u.data= odom_msg->twist.twist.linear.x;
-    current_v.data= odom_msg->twist.twist.linear.y;
-    current_w.data= odom_msg->twist.twist.linear.z;
-    current_p.data= odom_msg->twist.twist.angular.x;
-    current_q.data= odom_msg->twist.twist.angular.y;
-    current_r.data= odom_msg->twist.twist.angular.z;
-}
+        auto timer_callback = std::bind(&Odom_listener::publish_feedback, this);
+      timer = nh_->create_wall_timer(std::chrono::milliseconds((int)(100)), timer_callback);
 
-int main(int argc, char** argv){
+      feedback_pitch = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_PITCH_TOPIC, 10);
+    feedback_roll = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_ROLL_TOPIC, 10);
+    feedback_yaw = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_YAW_TOPIC, 10);
+    feedback_depth = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_DEPTH_TOPIC, 10);
+    feedback_x = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_X_TOPIC, 10);
+    feedback_y = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_Y_TOPIC, 10);
+    feedback_u = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_U_TOPIC, 10);
+    feedback_v = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_V_TOPIC, 10);
+    feedback_w = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_W_TOPIC, 10);
+    feedback_p = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_P_TOPIC, 10);
+    feedback_q = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_Q_TOPIC, 10);
+    feedback_r = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_R_TOPIC, 10);
+    feddback_alt = nh_->create_publisher<std_msgs::msg::Float64>(dead_reckoning_msgs::msg::Topics::DR_ODOM_ALT_TOPIC, 10);
 
-  std::string node_name = "odom_listener";
-  rclcpp::init(argc, argv);
+    }
+    void DVLCallback(const smarc_msgs::msg::DVL::SharedPtr dvl_msg)
+    {
+        current_alt.data = dvl_msg->altitude;
+    }
+    
+      void OdomCallback(const nav_msgs::msg::Odometry::SharedPtr odom_msg)
+    {
+        tf2::fromMsg(odom_msg->pose.pose.orientation, tfq);
+    
+        tf2::Matrix3x3(tfq).getEulerYPR(y,p,r);
+        //orientation
+        current_pitch.data= p;
+        current_roll.data= r;
+        current_yaw.data= y;
+        current_depth.data= -odom_msg->pose.pose.position.z;
+        current_x.data= odom_msg->pose.pose.position.x;
+        current_y.data= odom_msg->pose.pose.position.y;
+    
+        //Velocity
+        current_u.data= odom_msg->twist.twist.linear.x;
+        current_v.data= odom_msg->twist.twist.linear.y;
+        current_w.data= odom_msg->twist.twist.linear.z;
+        current_p.data= odom_msg->twist.twist.angular.x;
+        current_q.data= odom_msg->twist.twist.angular.y;
+        current_r.data= odom_msg->twist.twist.angular.z;
 
-  auto node = std::make_shared<rclcpp::Node>(node_name);
-
-  std::string odom_topic_;
-  std::string dvl_topic_;
-  double freq;
-
-  auto node_priv = std::make_shared<rclcpp::Node>("odom_listener");
-  node_priv->declare_parameter<std::string>("odom_topic", "/sam/dr/local/odom/filtered");
-  node_priv->declare_parameter<std::string>("dvl_topic", "/sam/core/dvl");
-  node_priv->declare_parameter<double>("loop_freq", 10);
-
-  node_priv->get_parameter("odom_topic", odom_topic_);
-  node_priv->get_parameter("dvl_topic", dvl_topic_);
-  node_priv->get_parameter("loop_freq", freq);
-
-  auto dvl_sub = node->create_subscription<smarc_msgs::msg::DVL>(dvl_topic_, 10, DVLCallback);
-  auto odom_sub = node->create_subscription<nav_msgs::msg::Odometry>(odom_topic_, 10, OdomCallback);
-  
-  auto feedback_pitch = node->create_publisher<std_msgs::msg::Float64>("/pitch", 10);
-    auto feedback_roll = node->create_publisher<std_msgs::msg::Float64>("/roll", 10);
-    auto feedback_yaw = node->create_publisher<std_msgs::msg::Float64>("/yaw", 10);
-    auto feedback_depth = node->create_publisher<std_msgs::msg::Float64>("/depth", 10);
-    auto feedback_x = node->create_publisher<std_msgs::msg::Float64>("/x", 10);
-    auto feedback_y = node->create_publisher<std_msgs::msg::Float64>("/y", 10);
-    auto feedback_u = node->create_publisher<std_msgs::msg::Float64>("/u", 10);
-    auto feedback_v = node->create_publisher<std_msgs::msg::Float64>("/v", 10);
-    auto feedback_w = node->create_publisher<std_msgs::msg::Float64>("/w", 10);
-    auto feedback_p = node->create_publisher<std_msgs::msg::Float64>("/p", 10);
-    auto feedback_q = node->create_publisher<std_msgs::msg::Float64>("/q", 10);
-    auto feedback_r = node->create_publisher<std_msgs::msg::Float64>("/r", 10);
-    auto feddback_alt = node->create_publisher<std_msgs::msg::Float64>("/alt", 10);
-    rclcpp::Rate rate(10.0);
-    while(rclcpp::ok())
+    }
+        void publish_feedback(){
+    // rclcpp::Rate rate(10.0);
+    if(rclcpp::ok())
     {
         feedback_pitch->publish(current_pitch);
         feedback_roll->publish(current_roll);
@@ -88,10 +110,26 @@ int main(int argc, char** argv){
         feedback_q->publish(current_q);
         feedback_r->publish(current_r);
         feddback_alt->publish(current_alt);
-        ROS_INFO_THROTTLE(1.0, "[ odom_listener ] roll: %f, pitch: %f, yaw: %f, depth: %f, x: %f, y: %f, u: %f, v: %f, w: %f, p: %f, q: %f, r: %f alt:%f", current_roll.data,current_pitch.data,current_yaw.data,current_depth.data, current_x.data, current_y.data, current_u.data, current_v.data, current_w.data, current_p.data, current_q.data, current_r.data, current_alt.data);
-
-        rate.sleep();
-        rclcpp::spin_some(node);
+     }
     }
+};
+
+
+int main(int argc, char** argv){
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("Odom_listener");
+
+  Odom_listener* odom_obj = new Odom_listener(*node);
+
+  rclcpp::Rate loop_rate(10);
+  while(rclcpp::ok())
+  {
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+  }
+
+  delete odom_obj;
+  RCLCPP_INFO(node->get_logger(), "odom listener finished");
+
   return 0;
 }
